7adf92ff2967848853ad16a04b26afa6d5a3ec61
demingongo <sdemingongo@gmail.com>
2025-05-02 21:30:23 +0200

    controller + Controller interface

--- src/utils/routingUtils.ts
+++ src/utils/routingUtils.ts
@@ -2,12 +2,41 @@ import routing from '@novice1/routing'
 import * as core from 'express-serve-static-core';
 import { ParsedQs } from 'qs'
 
+export interface Controller<
+    P = core.ParamsDictionary,
+    ResBody = unknown,
+    ReqBody = unknown,
+    ReqQuery = ParsedQs,
+    Locals extends Record<string, unknown> = Record<string, unknown>,
+    MetaResType = unknown
+> {
+    (
+        req: routing.Request<P, ResBody, ReqBody, ReqQuery, Locals, MetaResType>,
+        res: core.Response<ResBody, Locals>,
+        next: core.NextFunction,
+    ): ResBody | void;
+}
+
 export function controller<P = core.ParamsDictionary,
     ResBody = unknown,
     ReqBody = unknown,
     ReqQuery = ParsedQs,
     Locals extends Record<string, unknown> = Record<string, unknown>,
     MetaResType = unknown
->(handler: routing.RequestHandler<P, ResBody, ReqBody, ReqQuery, Locals, MetaResType>): routing.RequestHandler<P, ResBody, ReqBody, ReqQuery, Locals, MetaResType> {
-    return handler
+>(handler: Controller<P, ResBody, ReqBody, ReqQuery, Locals, MetaResType>): routing.RequestHandler<P, ResBody, ReqBody, ReqQuery, Locals, MetaResType> {
+    return async function (req, res, next) {
+        const value = await handler(req, res, next)
+        if (typeof value != 'undefined' && !res.headersSent) {
+            if (!res.hasHeader('Content-Type')) {
+                // there is no 'Content-Type' header, we have to guess how to return the content
+                if (typeof value === 'object') {
+                    return res.json(value)
+                } else if (typeof value === 'string') {
+                    return res.setHeader('Content-Type', 'text/plain').send(value)
+                }
+            }
+            return res.send(value)
+        }
+        return
+    }
 }
