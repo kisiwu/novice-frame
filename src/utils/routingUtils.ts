c95910124ba3e98ada7a368ba9f5c7dd2f9651cb
demingongo <sdemingongo@gmail.com>
2025-05-03 18:22:09 +0200

    controller: stream

--- src/utils/routingUtils.ts
+++ src/utils/routingUtils.ts
@@ -1,5 +1,6 @@
 import routing from '@novice1/routing'
 import * as core from 'express-serve-static-core';
+import Stream, { Readable } from 'stream';
 import { ParsedQs } from 'qs'
 
 export interface Controller<
@@ -14,7 +15,7 @@ export interface Controller<
         req: routing.Request<P, ResBody, ReqBody, ReqQuery, Locals, MetaResType>,
         res: core.Response<ResBody, Locals>,
         next: core.NextFunction,
-    ): ResBody | void | Promise<ResBody> | Promise<void>;
+    ): ResBody | void | core.Response<ResBody, Locals> | Promise<ResBody> | Promise<void> | Promise<core.Response<ResBody, Locals>>;
 }
 
 export function controller<P = core.ParamsDictionary,
@@ -27,6 +28,18 @@ export function controller<P = core.ParamsDictionary,
     return async function (req, res, next) {
         const value = await handler(req, res, next)
         if (typeof value != 'undefined' && !res.headersSent) {
+
+            // if stream
+            if (value instanceof Stream) {
+                // the stream must be readable
+                if (value instanceof ReadableStream || value instanceof Readable) {
+                    return value.pipe(res)
+                }
+                // do nothing
+                return
+            }
+
+            // if no 'Content-Type' header
             if (!res.hasHeader('Content-Type')) {
                 // there is no 'Content-Type' header, we have to guess how to return the content
                 if (typeof value === 'object') {
@@ -35,8 +48,11 @@ export function controller<P = core.ParamsDictionary,
                     return res.setHeader('Content-Type', 'text/plain').send(value)
                 }
             }
+
+            // simply send the content
             return res.send(value)
         }
+        // do nothing
         return
     }
 }
