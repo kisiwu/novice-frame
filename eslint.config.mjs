45337ddc3f082f38cb795382f4be104052f663c5
demingongo <sdemingongo@gmail.com>
2025-06-19 13:11:00 +0200

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
-import stylisticJs from '@stylistic/eslint-plugin-js'
+import stylisticJs from '@stylistic/eslint-plugin'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
